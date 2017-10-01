9f535b8003c66f7eb7c6f221684517e9f8c06c06
Christopher Crouzet <christopher.crouzet@gmail.com>
2017-10-01 17:20:58 +0700

    Enforce full URLs for medias

--- themes/default/templates/functions/_.js
+++ themes/default/templates/functions/_.js
@@ -1,13 +1,31 @@
+const { tree } = require('../../../../lib/common');
 const { toHTML, makeMediaNode } = require('../../../../lib/html');
+const {
+  walkTreeSync,
+  getNodeURIAttrs,
+  resolveFullURI,
+} = require('../../../../lib/utils');
+
+
+const MEDIA_TAGS = ['img', 'video'];
 
 
 
 // Render a media defined in the meta.
-function renderMedia(file) {
+function renderMedia(file, files) {
   return () => (text, render) => {
     const meta = file.meta.medias[text];
-    const node = makeMediaNode(meta.size, meta);
-    return toHTML(node);
+    const mediaNode = makeMediaNode(meta.size, meta);
+    walkTreeSync(mediaNode, (node) => {
+      if (MEDIA_TAGS.includes(tree.getTagName(node))) {
+        getNodeURIAttrs(node)
+          .forEach((attr) => {
+            attr.value = resolveFullURI(attr.value, file.meta, files);
+          });
+      }
+    });
+
+    return toHTML(mediaNode);
   };
 }
 
